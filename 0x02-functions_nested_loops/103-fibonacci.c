eba875111d522809eef94d4a91a8c42d21cccc63
JuanDavidPena <j_david0915@hotmail.com>
2021-06-23 20:00:33 +0000

    indents

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,23 +7,24 @@
  */
 int main(void)
 {
-int i;
-unsigned long int j, k, next, sum;
-j = 1;
-k = 2;
-sum = 0;
+	int i;
+	unsigned long int j, k, next, sum;
 
-for (i = 1; i <= 33; ++i)
-{
-if (j < 4000000 && (j % 2) == 0)
-{
-sum = sum + j;
-}
-next = j + k;
-j = k;
-k = next;
-}
-printf("%lu\n", sum);
+	j = 1;
+	k = 2;
+	sum = 0;
 
-return (0);
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+		sum = sum + j;
+		}
+		next = j + k;
+		 = k;
+		k = next;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
 }
