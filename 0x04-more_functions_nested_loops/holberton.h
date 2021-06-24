2c289dbee12fa2a092ac3bf3df20d5eae986361c
JuanDavidPena <j_david0915@hotmail.com>
2021-06-24 18:18:03 +0000

    adding putchar

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 int _isupper (int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,6 +11,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 
 
 #endif
