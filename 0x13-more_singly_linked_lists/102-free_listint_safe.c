f8820e8553fb3dbffec2ccd616fef5eaab3d9b55
JuanDavidPena <j_david0915@hotmail.com>
2021-08-03 16:31:35 +0000

    [modify]- task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,53 +5,6 @@ size_t looped_list_unique(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
 /**
- * looped_list_unique - Counts the number of unique nodes
- *                      in a looped listint_t linked list.
- * @head: A pointer to the start of the listint_t.
- * Return: If the list is not looped - 0.
- *         Or the number of unique nodes in the list.
- */
-size_t looped_list_unique(const listint_t *head)
-{
-	const listint_t *a, *b;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	a = head->next;
-	b = (head->next)->next;
-
-	while (b)
-	{
-		if (a == b)
-		{
-			a = head;
-			while (a != b)
-			{
-				nodes++;
-				a = a->next;
-				b = b->next;
-			}
-
-			a = a->next;
-			while (a != b)
-			{
-				nodes++;
-				a = a->next;
-			}
-
-			return (nodes);
-		}
-
-		a = a->next;
-		b = (b->next)->next;
-	}
-
-	return (0);
-}
-
-/**
  * free_listint_safe - Frees a listint_t list that could have loops.
  * @h: A pointer to the address of
  *     the head of the listint_t list.
